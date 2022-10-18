052262abfb271d89c1c1c30f2665cd243d5ad924
mhilmeeali <m.hilmee@gmail.com>
2022-10-18 16:39:38 +0800

    Commit myFinalProject to Github.

---
+++ myFinalProject/src/main/java/com/example/joy/myFinalProject/SpringBootRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.joy.myFinalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.example.joy"})
+public class SpringBootRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestApiApplication.class, args);
+    }
+}
